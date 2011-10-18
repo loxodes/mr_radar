731ec27e7b423b68adff1b3edda297266bafb6fb
Jon Klein <kleinjt@ieee.org>
2011-10-18 13:45:13 -0800

    added visualization of output

--- software/vco_correction/radar_vco_test.m
+++ software/vco_correction/radar_vco_test.m
@@ -4,9 +4,10 @@ f_stop = 20e6;
 t_sweep = 1e-3;
 type = 'ideal';
 ts = 1e-8;
-tbounce = 10e-5;
-f_cutoff = 10e6;
+tbounce = 2e-5;
+f_cutoff = 1e6;
 
 [ t, f_rf ] = vco( f_start, f_stop, t_sweep, type, ts );
 f_if = delay_line(f_rf, tbounce, ts);
 [ f_lo ] = mixer( f_rf, f_if, f_cutoff, ts );
+spectrogram(f_lo,256,250,256,1/ts);
