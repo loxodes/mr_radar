d15446e4262f275c4b19ae722207660afa2faa3d
Jon Klein <kleinjt@ieee.org>
2011-10-21 14:33:24 -0800

    added fft output visualization

--- software/vco_correction/radar_vco_test.m
+++ software/vco_correction/radar_vco_test.m
@@ -2,13 +2,14 @@
 f_start = 30e6;         % start frequency (Hz)
 f_stop = 35e6;          % stop frequency (Hz)
 t_sweep = 1e-3;         % sweep time (s)
-type = 'ideal';         % vco type ('ideal', 'gnoise')
+type = 'awgnoise';      % vco type ('ideal', 'awgnoise')
+vco_snr = 5;            % vco snr for awgnoise (dB)
 ts = 1e-8;              % sample time
 tbounce = 20e-5;        % target distance (s)
-f_cutoff = 10e6;        % mixer filter cutoff frequency (Hz)
+f_cutoff = 2e6;         % mixer filter cutoff frequency (Hz)
 
 % create time and frequency vectors for VCO
-[ t, f_rf ] = vco( f_start, f_stop, t_sweep, type, ts );
+[ t, f_rf ] = vco( f_start, f_stop, t_sweep, type, ts, vco_snr);
 
 % create IF frequency, delayed version of RF
 f_if = delay_line(f_rf, tbounce, ts);
@@ -17,12 +18,23 @@ f_if = delay_line(f_rf, tbounce, ts);
 [ f_lo ] = mixer( f_rf, f_if, f_cutoff, ts );
 
 % display spectrogram of downconverted output
-subplot(3,1,1);
+subplot(4,1,1);
 spectrogram(f_lo,256,250,256,1/ts);
 title('sampled signal');
-subplot(3,1,2);
+
+% display one sided spectrum of f_lo up to mixer cutoff
+subplot(4,1,2);
+nfft = 2^nextpow2(length(f_lo));
+fft_lo = fft(f_lo,nfft);
+f_fft_lo = linspace(0,1,nfft/2+1)/(2*ts);
+f_lo_lp = f_fft_lo(f_fft_lo < f_cutoff);
+plot(f_lo_lp, 2*abs(fft_lo(1:length(f_lo_lp))));
+grid on;
+
+% display spectrogram of transmitted and received chirps
+subplot(4,1,3);
 spectrogram(f_rf,256,250,256,1/ts);
 title('transmitted chirp');
-subplot(3,1,3);
+subplot(4,1,4);
 spectrogram(f_if,256,250,256,1/ts);
 title('received chirp');
--- software/vco_correction/vco.m
+++ software/vco_correction/vco.m
@@ -1,15 +1,20 @@
-function [ t, fout ] = vco( f_start, f_stop, t_sweep, type, ts )
+function [ t, fout ] = vco( f_start, f_stop, t_sweep, type, ts, noise)
     % generates simulated VCO output
-    % currently only works for an ideal vco
-    if(~strcmp(type,'ideal'))
-        warning('vco: sorry, only ideal vcos are currently supported'); %#ok<WNTAG>
-    end
+    % currently only works for an ideal or awgn vco
     
     df = (f_stop-f_start)/(t_sweep/ts);     % calculate frequency step per sample time
     f = f_start:df:f_stop;                  % calculate instantaneous frequency of chrip
     pa = 2*pi*f*ts;                         % calculate phase change between time steps
     p = mod(cumsum(pa),2*pi);               % calculate phase of cosine at each timestep
     t = 0:ts:t_sweep;                       % create time vector
-    fout = cos(p);                          % calculate amplitude of time signal from phase
+    
+    % calculate amplitude of time signal from phase
+    if(strcmp(type,'ideal'))
+        fout = cos(p);
+    elseif strcmp(type, 'awgnoise')
+        fout = awgn(cos(p),noise);
+    else
+         warning('vco: sorry, only ideal and awgnoise vcos are currently supported'); %#ok<WNTAG>
+    end
 end
 
