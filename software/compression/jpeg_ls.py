4a19569b7143e9c29b9ef19fe06d72767ce705ab
Jon Klein <kleinjt@ieee.org>
2012-04-28 14:32:59 -0800

    more work on jpeg_ls, still almost certainly broken

--- software/compression/jpeg_ls.py
+++ software/compression/jpeg_ls.py
@@ -1,5 +1,6 @@
-# jpeg-ls encoder/decoder
-# pass in list of lists
+# jpeg-ls encoder
+# almost certainly not standards compliant, and entirely unoptimized
+# 
 # uses notation and algorithm from: 
 # and The LOCO-I Lossless Image Compression Algorithm: Principles and Standardization into JPEG-LS
 # Marcelo J. Weinberger, Gadiel Seroussi, and Guillermo Sapiro
@@ -12,15 +13,18 @@
 #  a  x  <-- current pixel
 
 import numpy
+import pdb
+import sys
 
 quan_vector = [-15,-7,-3,1,0,1,3,7,15] # quantization steps for q1, q2, q3
-quan_level = 5 # quantization level for q4
-bias_table = {}
 
 A_CONTEXT = 0
 B_CONTEXT = 1
 C_CONTEXT = 2
 N_CONTEXT = 3
+INT_CONTEXT_IDX = [365, 366]
+
+J = [0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,5,5,6,6,7,7,8,9,10,11,12,13,14,15]
 
 N0 = 64 # reset threshold for N, set between 32 and 256
 
@@ -36,16 +40,18 @@ def jpegls_encode(image, bpp):
     alpha = pow(2, bpp)
 
     # initialize context table, [A, B, C, N]
-    context_table = [[max(2, numpy.floor(((alpha+32)/64))),0,0,1] for i in range(365)]
-    
+    context_table = [[max(2, numpy.floor(((alpha+32)/64))),0,0,1] for i in range(367)]
+    NN = [0, 0]
+
     # initialize irun
     irun = 0
-    m = 8 # !!! what should this init to? 
+    
     for row in range(height): 
         output.append([])
         run = 0
-        
-        for col in range(width):
+        col = 0 
+        while col < width:
+            print 'encoding: ' + str(row) + ',' + str(col)
             # determine context pixels, with special cases: first and last column, first row
             x = image[row][col]
             sign = 1
@@ -67,39 +73,96 @@ def jpegls_encode(image, bpp):
             else:
                 a = image[row][col-1]
             
-            # check for run mode processing
-            if(run):
-                if(run == m):
-                    output[-1].append('1')
-                    run = 0
-                    irun += 1
-                    m = updatem(irun, m)
-
-                if x == a:
-                    run += 1
-                    if(col == width - 1):
-                        output[-1].append('1')
-                        irun += 1
-                        # update m run table?
-                    continue
-                else:
-                    output[-1].append('0' + bin(run)[2:])
-                    irun -= 1
-                    run = 0
-                    m = updatem(irun, m)
-            
-            # otherwise.. single component encoding
-                        # step 1, compute local gradients
+            # step 1, compute local gradients
             g1 = d - b
-            g2 = a - c
-            g3 = c - b
+            g2 = b - c #a - c ???
+            g3 = c - a #c - b ???
             
             # step 2, check for run mode processing
             if g1 == g2 == g3 == 0:
-                run += 1
-                continue
+                print 'run mode processing'
+                run = 0
+                x_run = x
+
+                while x_run == x:
+                    run += 1
+                    if col + run >= width - 1:
+                        break
+                    x = image[row][col+run]
+
+                col += run
+
+                while run >= (1 << J[irun]):
+                    output[-1].append('1')
+                    run -= (1 << J[irun])
+                    if irun <= len(J):
+                        irun += 1
+                
+                if x_run != x:
+                    output[-1].append('0' + bin(run)[2:])
+                    if irun > 0:
+                        irun -= 1
+                    
+                    # encode residual using interruption context
+                    a = image[row][col-1]
+                    if row:
+                        b = image[row-1][col]
+                    
+                    int_type = a - b == 0
+                     
+                    if int_type:
+                        x_hat = a
+                    else:
+                        x_hat = b
+
+                    r = x - x_hat
+                     
+                    if int_type and a > b:
+                        x_hat = -x_hat
+                        sign = -1
+                    
+                    r = clamp_range(r, alpha)
+
+                    A = context_table[INT_CONTEXT_IDX[int_type]][A_CONTEXT]
+                    B = context_table[INT_CONTEXT_IDX[int_type]][B_CONTEXT]
+                    C = context_table[INT_CONTEXT_IDX[int_type]][C_CONTEXT]
+                    N = context_table[INT_CONTEXT_IDX[int_type]][N_CONTEXT]
+    
+                    t = a + int_type * (N >> 1)    
+                 
+                    k = compute_k(N, t)
+                    
+                    if (k == 0) and (r < 0) and (2 * NN[int_type] < N): 
+                        map = 1
+                    elif (r < 0) and (2 * NN[int_type] >= N):
+                        map = 1
+                    elif (r < 0) and k:
+                        map = 1
+                    else:
+                        map = 0
+
+                    r_map = 2 * abs(r) - int_type - map
+                    
+                    output[-1].append(gpo2_encode(k, r_map)) # ADD LIMITING
+
+                    if r < 0:
+                        NN[int_type] += 1
+                    
+                    A += (r_map + int_type) >> 1
+                    if N == N0:
+                        N = N / 2
+                        A = A / 2
+                        NN[int_type] = NN[int_type] / 2
+                    
+                    N += 1
+                    context_table[INT_CONTEXT_IDX[int_type]][A_CONTEXT] = A
+                    context_table[INT_CONTEXT_IDX[int_type]][N_CONTEXT] = N
+
+                else: # end of line
+                    output[-1].append('1')
 
             else:
+                print 'local encoding'
                 # step 3, quantize the local gradients 
                 Q = vect_quantize([g1, g2, g3], quan_vector)
                 
@@ -114,9 +177,11 @@ def jpegls_encode(image, bpp):
                         continue
                     else:
                         break
+
                 # compute context index, gather contexts
                 context = 81 * Q[0] + 9 * Q[1] + Q[2]
-                
+                print Q
+                print context 
                 A = context_table[context][A_CONTEXT]
                 B = context_table[context][B_CONTEXT]
                 C = context_table[context][C_CONTEXT]
@@ -135,23 +200,18 @@ def jpegls_encode(image, bpp):
                 
                 if x_hat < 0:
                     x_hat = 0
-                if x_hat > alpha - 1:
+                elif x_hat > alpha - 1:
                     x_hat = alpha - 1
                 
                 # step 7, compute prediction residual
                 r = sign * (x - x_hat)
                 
                 # this doesn't entirely make sense yet.. stepping by alpha feels extreme
-                while r <= numpy.floor(-alpha/2):
-                    r += alpha
-                while r > numpy.ceil(alpha/2) + 1:
-                    r -= alpha
+                r = clamp_range(r, alpha)
                 
                 # step 8, compute golumn parameter k
-                k = 0
-                while (N << k) < A:
-                    k += 1
-                
+                k = compute_k(N, A)
+
                 # step 9, map residual
                 if k == 0 and 2 * B <= -N:
                     if r >= 0:
@@ -165,10 +225,14 @@ def jpegls_encode(image, bpp):
                         r_map = -2 * r - 1
 
                 # step 10, golomb encode prediction residual using k
-                output[-1].append(gpo2_encode(m, r_map)) # add limiting to l_max!
+                if r_map >> k < l_max - bpp - 1:
+                    output[-1].append(gpo2_encode(k, r_map))
+                else:
+                    word = bin(r_map)[2:]
+                    word = '0' * (bpp - len(word)) + word
+                    output[-1].append('0' * (l_max - bpp - 1) + '1' + word)
 
                 # step 11 and 12, update context counters 
-
                 A += abs(r)
                 B += r
                 
@@ -194,29 +258,43 @@ def jpegls_encode(image, bpp):
                 context_table[context][B_CONTEXT] = B
                 context_table[context][C_CONTEXT] = C
                 context_table[context][N_CONTEXT] = N
+                col += 1
+
     return output
 
+def clamp_range(r, alpha):
+    while r <= numpy.floor(-alpha/2):
+        r += alpha
+    print 'r: ' + str(r)
+    while r > numpy.ceil(alpha/2) + 1:
+        r -= alpha
+    return r
+                  
+def compute_k(N, A):
+    k = 0
+    while (N << k) < A:
+        k += 1
+    return k
+ 
 def vect_quantize(g_vect, vector):
-    return [[g > v for v in vector].index(False)-1 for g in g_vect]
+    q = [0,0,0]
+    
+    for i in range(len(g_vect)):
+        cmp = [g_vect[i] < v for v in vector] 
+        if True in cmp:
+            q[i] = cmp.index(True)-4
+        else:
+            q[i] = 4
+    return q 
 
-def thresh_quantize(g, threshold):
-    if(abs(g) < threshold):
-        return 0
-    if(g > threshold):
-        return 1
-    return 2
-
-def gpo2_encode(m, codeword):
-    quotient = unary(codeword / m)
-    remainder = bin(codeword % m)[2:]
+def gpo2_encode(k, codeword):
+    if k:
+        quotient = unary(codeword / k)
+    else:
+        quotient = ''
+        k = pow(2,16) # FIX !!
+    remainder = bin(codeword % k)[2:]
     return quotient + remainder
 
 def unary(x):
     return x * '0' + '1'
-
-def jpegls_decode(image):
-    pass
-
-def updatem(irun, m):
-    print 'updating run (STUB)'
-    return m
